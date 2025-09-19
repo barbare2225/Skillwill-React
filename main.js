94e234fbc639fca6e696c34c9275dc962d02f829
barbare2225 <161972787+barbare2225@users.noreply.github.com>
2025-09-19 11:34:53 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// 1
 function calculate(a,b){
     if(typeof a!=="number"){
         return "შეცდომა მატრიცაში"
@@ -5,21 +6,22 @@ function calculate(a,b){
     if(typeof  b!=="number"){
         return "შეცდომა მატრიცაში"
     }
-    return a+b
-    
+    return a+b 
 }
 
+// 2
 function temperature_from_f_to_c(temperature){
     if(typeof temperature!=="number") return false
     return (temperature-32)/1.8000
 }
 
+// 3
 function calculate2(a,b,operation){
     if(typeof a !== "number") {
         return false
     } if(typeof b !== "number"){
         return false
-    }if(operation == "+") {
+    } if(operation == "+") {
         return a+b
     } if(operation == "-") {
         return a-b
@@ -30,8 +32,3 @@ function calculate2(a,b,operation){
     }
     return false
 }
-let c=4543
-let d=true
-const answer=temperature_from_f_to_c(c)
-
-console.log(answer)
